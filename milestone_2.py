335d47809a21daf8a21edf03a52e277d7e55c730
Harj95 <150259377+Harj95@users.noreply.github.com>
2023-11-17 16:13:13 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -2,16 +2,12 @@
 favorite_fruits = ["Apple", "Banana", "Orange", "Grapes", "Watermelon"]
 word_list = favorite_fruits
 
-print("My favorite fruits list:", word_list)
-
 #Task 2
 import random
 
 word_list = ["Apple", "Banana", "Orange", "Grapes", "Watermelon"]
 random_word = random.choice(word_list)
 word = random_word
-
-print("Randomly selected word:", word)
 
 #Task 3
 guess = input("Enter a letter: ")
